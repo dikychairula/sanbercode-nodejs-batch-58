db99fef95461ef2739ffcace8fecded4cdc3602c
dikychairul <dikychairula@gmail.com>
2024-07-24 14:41:20 +0700

    tugas7, added multiple upload

--- tugas7-middleware-express/src/route.ts
+++ tugas7-middleware-express/src/route.ts
@@ -1,24 +1,15 @@
 import express from "express";
-import multer from "multer";
 import { single, multiple } from "./middlewares/upload.middleware";
 import { handleUpload } from "./utils/cloudinary";
 
 const router = express.Router();
-const cekRoute = [
-    {name: 'pindang'},
-    {name: 'bokir'}
-];
-
-router.get("/cek", (req, res) => {
-    res.send(cekRoute);
-});
 
 router.post("/upload/single", single, async(req, res) => {
     if (!req.file) {
         return res.status(400).send('No File uploaded');
     }
     try {
-        const result = await handleUpload(req.file.buffer); // Menggunakan buffer dari multer
+        const result = await handleUpload(req.file.buffer);
         res.status(200).send(result);
     } catch (error) {
         console.error('Error uploading to Cloudinary:', error);
@@ -26,9 +17,19 @@ router.post("/upload/single", single, async(req, res) => {
     }
 });
 
-router.post("/upload/multiple", multiple, (req, res) => {
-    
+router.post("/upload/multiple", multiple, async (req, res) => {
+    if (!req.files) {
+        return res.status(400).send('No Files uploaded');
+    }
+    try {
+        //upload each buffer to cloudinary using Promise.all
+        const promises = (req.files as Express.Multer.File[]).map(file => handleUpload(file.buffer));
+        const results = await Promise.all(promises);
+        res.status(200).send(results);
+    } catch (error) {
+        console.error('Error uploading to Cloudinary:', error);
+        res.status(500).send('Error uploading to Cloudinary');
+    }
 });
-
 
 export default router;
